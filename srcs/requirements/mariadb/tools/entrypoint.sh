57674a708022ebe1d8a684862e783738e3386a61
user <ekaitzlopez2025@gmail.com>
2026-02-13 11:59:33 +0100

    Update

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -11,16 +11,15 @@ fi
 mysqld_safe &
 
 echo "Waiting for MariaDB to start..."
-until mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" ping --silent; do
+until mysqladmin -u root -p "$MYSQL_ROOT_PASSWORD" ping --silent; do
     sleep 1
 done
 echo "MariaDB started."
 
 echo "Running initial database setup..."
-
 mysqladmin -u root password "$MYSQL_ROOT_PASSWORD" || true
 
-mariadb -u root -p"$MYSQL_ROOT_PASSWORD" << EOF
+mariadb -u root -p "$MYSQL_ROOT_PASSWORD" << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 
@@ -29,9 +28,8 @@ CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 echo "Initial database setup completed."
 
-mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+mysqladmin -u root -p "$MYSQL_ROOT_PASSWORD" shutdown
 
 exec mysqld
